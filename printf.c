453e719e79bbeef618f79e761151ea00bf0e8e18
Totolosa <thomas.dayde@gmail.com>
2020-12-10 12:08:04 +0100

    fin journee 09/12/20

--- printf.c
+++ printf.c
@@ -6,30 +6,32 @@
 /*   By: tdayde <tdayde@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 11:42:32 by tdayde            #+#    #+#             */
-/*   Updated: 2020/12/08 22:43:30 by tdayde           ###   ########lyon.fr   */
+/*   Updated: 2020/12/09 18:53:58 by tdayde           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-void type_conversion(char c, t_info_flags *flags)
+void type_conversion(t_info_resolution *table, char c)
 {
 	if (c == 'c')
-		flags->type = 'c';
+		table->type = 'c';
 	if (c == 's')
-		flags->type = 's';
+		table->type = 's';
 	if (c == 'p')
-		flags->type = 'p';
+		table->type = 'p';
 	if (c == 'd')
-		flags->type = 'd';
+		table->type = 'd';
 	if (c == 'i')
-		flags->type = 'i';
+		table->type = 'i';
 	if (c == 'u')
-		flags->type = 'u';
+		table->type = 'u';
 	if (c == 'x')
-		flags->type = 'x';
+		table->type = 'x';
 	if (c == 'X')
-		flags->type = 'X';
+		table->type = 'X';
+	if (c == '%')
+		table->type = '%';
 }
 
 // void type_conversion(char *str, int i, va_list *arg)
@@ -50,87 +52,107 @@ void type_conversion(char c, t_info_flags *flags)
 // 		va_arg(arg, int);
 // 	if (str[i] == 'X')
 // 		va_arg(arg, int);
+//	if (str[i] == '%')
+// 		va_arg(arg, int);
 // }
 
-
-t_info_flags reconize_flags(char c, t_info_flags *flags)
+void	reinitialize_table(t_info_resolution *table)
 {
-	if (c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i' || c == 'u' || c == 'x' || c == 'X')
-		type_conversion(c, flags);
-	return(*flags);
+	table->flags = 0;
+	table->width = 0;
+	table->prec = 0;
+	table->type = '\0';
+	printf("table->type = %c\n", table->type);
 }
 
-//cspdiuxX
+void	reconize_flags(t_info_resolution *t, const char *s)
+{
+	if (s[t->i] == '-')
+		t->flags = '-';
+	if (s[t->i] == '0')
+		t->flags = '0';
+	t->i++;
+}
 
-// char	*printf_strdup(const char *s)
-// {
-// 	char	*dest;
-// 	int		i;
+void	reconise_width(t_info_resolution *t, const char *s)
+{
+	int width;
+	
+	width = 0;
+	if (s[t->i] >= '*')
+		t->width = -1;
+	while (s[t->i] >= '0' && s[t->i] <= '9')
+	{
+		width = width * 10 + s[t->i] - '0';
+		t->i++;
+	}
+		
+	if (s[t->i] == '0')
+		t->flags = '0';
+	t->i++;
+}
 
-// 	if ((dest = malloc(sizeof(char) * gnl_strlen(s) + 1)) == NULL)
-// 		return (NULL);
-// 	i = 0;
-// 	while (s[i])
-// 	{
-// 		dest[i] = s[i];
-// 		i++;
-// 	}
-// 	dest[i] = '\0';
-// 	return (dest);
-// }
+void	reconize_table(t_info_resolution *t, const char *s)
+{
+	reconize_flags(t, s);
 
-// char	*content_before_arg(char *buff, char c)
-// {
-// 	int		i;
-// 	int		j;
-// 	char	*res;
+	
+	if (s[t->i] == 'c' || s[t->i] == 's' || s[t->i] == 'p' || s[t->i] == 'd'
+		|| s[t->i] == 'i' || s[t->i] == 'u' || s[t->i] == 'x' || s[t->i] == 'X'
+		|| s[t->i] == '%')
+		type_conversion(t, s[t->i]);
+	t->i++;
+}
 
-// 	i = 0;
-// 	while (buff[i] != c && buff[i])
-// 		i++;
-// 	if ((res = malloc(sizeof(char) * (i + 1))) == NULL)
-// 		return (NULL);
-// 	j = -1;
-// 	while (++j < i)
-// 		res[j] = buff[j];
-// 	res[j] = 0;
-// 	return (res);
-// }
+void	print_noargs(t_info_resolution *table, const char *format)
+{
+	while (format[(table->i)] != '%' && format[(table->i)])
+	{
+		write(1, &format[table->i], 1);
+		table->printed++;
+		(table->i)++;
+	}
+	if (format[table->i])
+		(table->i)++;
+	if (format[(table->i)] == '%')
+	{
+		write(1, &format[table->i], 1);
+		table->printed++;
+		(table->i)++;
+		print_noargs(table, format);
+	}
+}
 
 int ft_printf(const char *format, ...)
 {
-	int i;
-	int printed_char;
-	t_info_flags flags;
+	t_info_resolution table;
 //	va_list arg;
 
-	i = 0;
-	printed_char = 0;
 //	va_start(args, format);
-//	t_info table = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, format};
+//	t_info_resolution table = {0, 0, 0, 0, 0, 0};
 //	printf("format apres initialisation dans struct = %s", table.format);
-	while (format[i])
+	while (format[(table.i)])
 	{
-		while(format[i] != '%')
-			i++;
-		i++;
-		flags = reconize_flags(format[i], &flags);
-		printf("type = %c, format[i] = %c\n", flags.type, format[i]);
-		
+		print_noargs(&table, format);
+		reconize_table(&table, format);
+		printf("flags =  || width =  || prec =  || type = %c || printed = %d || i = %d || format[i - 1] = %c\n", table.type, table.printed, table.i, format[table.i - 1]);
+		/* PRINT AVEC FLAGS*/
 
+	//	reinitialize_table(&table);
+	//	printf("flags =  || width =  || prec =  || type = %c || printed = %d || i = %d || format[i - 1] = %c\n", table.type, table.printed, table.i, format[table.i - 1]);
 	}
 //	va_end(arg);
-	return(printed_char);
+	return(table.printed);
 }
 
 int main()
 {
-	int a = 1;
-	int b = 10;
+	int a = 333;
+	int b = 9;
 	int c = 3;
-//	int d = 4;
+//	int d;
 	setbuf(stdout, NULL);
-	ft_printf("test\n %d", b);
-	printf("Bibli : %d, %10.1d, %d\n", a, b, c);
+//	ft_printf("ligne 1\nligne 2 100%%\nPremier appel %%d %uDeuxieme appel %%d %d");
+	printf("Bibli : %010d, %10%, %d\n", a, b, c);
 	return(0);
 }
--- printf.h
+++ printf.h
@@ -6,23 +6,27 @@
 /*   By: tdayde <tdayde@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 14:19:12 by tdayde            #+#    #+#             */
-/*   Updated: 2020/12/08 22:32:38 by tdayde           ###   ########lyon.fr   */
+/*   Updated: 2020/12/09 18:54:00 by tdayde           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINTF_H
 # define PRINTF_H
 
-#include <stdarg.h>
-#include <stdio.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
 
-typedef struct	s_info_flags
+typedef struct	s_info_resolution
 {
+	char flags;
+	int width;
+	char prec;
 	char type;
 	int printed;
-	int index;
-	const char *format;
-}				t_info_flags;
+	int i;
+//	const char *format;
+}				t_info_resolution;
 
 int ft_printf(const char *format, ...) __attribute__ ((format (printf, 1, 2)));
 
